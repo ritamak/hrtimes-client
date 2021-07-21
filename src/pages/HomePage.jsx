0cc7e789eaeffc0dc4e7f54cd8d3c33787a55b19
Halil Ibrahim Kaya <halilibrahim280@gmail.com>
2021-07-21 14:12:55 +0200

    landing page done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,29 @@
-import logo from "../logo.svg";
 import "../App.css";
+import {Link} from 'react-router-dom'
 
 function HomePage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to HR Times</h1>
+
+      <img src="/media/journal.png" />
+
+      <p>
+        HR Times is a platform to gather all topics of your interest. You can
+        easily choose the topics you are interested in and check them on your
+        feed! Join us and keep yourself posted!
+      </p>
+
+      <Link to="/signin">
+        <button>Sing In</button>
+      </Link>
+
+      <Link to="/signup">
+        <button>Sing Up</button>
+      </Link>
     </div>
   );
 }
 
 export default HomePage;
+                
