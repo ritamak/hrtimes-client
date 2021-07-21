a3ca847d51eecd8db4ddacfcd7a09657957b642f
ritamak <ritapmak@gmail.com>
2021-07-21 15:07:52 +0100

    merged halil and rita

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from "react";
-//import { Switch, Route } from "react-router-dom";
-
-function App() {
-  return <div className="App">Hello</div>;
-=======
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import HomePage from "./components/HomePage/HomePage";
 import SignIn from "./components/SignIn/SignIn";
-import { API_URL } from './config';
+import { API_URL } from "./config";
+import SignUp from "./components/signup/SignUp";
 
 function App(props) {
   const { user, updateUser } = useState(null);
-  const {myError, updateError } = useState(null);
+  const { myError, updateError } = useState(null);
 
   const handleSignIn = async (event) => {
     event.preventDefault();
@@ -25,26 +18,19 @@ function App(props) {
     let myUser = {
       email: email.value,
       password: password.value,
-    }
+    };
 
     try {
-      let response = await axios.post(`${API_URL}/api/signin`, myUser, { withCredentials: true })
-      
+      let response = await axios.post(`${API_URL}/api/signin`, myUser, {
+        withCredentials: true,
+      });
+
       updateUser(response.data);
       props.history.push("/profile");
     } catch (error) {
       updateError(error);
     }
-  }
-=======
-import React, { useState, useEffect } from "react";
-import { Switch, Route, withRouter } from "react-router-dom";
-import HomePage from "./components/HomePage/HomePage";
-import SignUp from "./components/signup/SignUp";
-import axios from "axios";
-
-function App(props) {
-  const { user, updateUser, newUser } = useState(null);
+  };
 
   const handleSignUp = async (event) => {
     event.preventDefault();
@@ -75,14 +61,22 @@ function App(props) {
       console.log("Signup failed", err);
     }
   };
->>>>>>> c4503f4162332be515e5f72fa5577628ca06c8cc
 
   return (
     <div className="App">
       <Switch>
-        <Route path="/signin" render={(routerProps) => {
-          return <SignIn error={myError} onSignIn={handleSignIn} {...routerProps}/>
-        }}/>
+        <Route
+          path="/signin"
+          render={(routerProps) => {
+            return (
+              <SignIn
+                error={myError}
+                onSignIn={handleSignIn}
+                {...routerProps}
+              />
+            );
+          }}
+        />
         <Route exact path="/" component={HomePage} />
         <Route
           path="/signup"
@@ -93,11 +87,6 @@ function App(props) {
       </Switch>
     </div>
   );
->>>>>>> 4775f01087c0c2076d71130bc4c23997e5202d9c
 }
 
-<<<<<<< HEAD
 export default withRouter(App);
-=======
-export default withRouter(App);
->>>>>>> c4503f4162332be515e5f72fa5577628ca06c8cc
