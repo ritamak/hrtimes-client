53a2cbd8a8a2ea38cbf170123078cf821dc0c9ba
Halil Ibrahim Kaya <halilibrahim280@gmail.com>
2021-07-22 09:47:58 +0200

    daily pull

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { API_URL } from "./config";
 import SignUp from "./components/signup/SignUp";
 
 function App(props) {
-  const { user, updateUser } = useState(null);
-  const { myError, updateError } = useState(null);
+  const [ user, updateUser ] = useState(null);
+  const [ myError, updateError ] = useState(null);
 
   const handleSignIn = async (event) => {
     event.preventDefault();
@@ -21,9 +21,7 @@ function App(props) {
     };
 
     try {
-      let response = await axios.post(`${API_URL}/api/signin`, myUser, {
-        withCredentials: true,
-      });
+      let response = await axios.post(`${API_URL}/api/signin`, myUser, {withCredentials: true});
 
       updateUser(response.data);
       props.history.push("/profile");
@@ -34,6 +32,7 @@ function App(props) {
 
   const handleSignUp = async (event) => {
     event.preventDefault();
+
     const {
       username,
       firstName,
@@ -55,35 +54,24 @@ function App(props) {
     };
 
     try {
-      await axios.post(`http://localhost:5005/api/signup`, newUser);
-      props.history.push("/");
+      await axios.post(`${API_URL}/api/signup`, newUser, {withCredentials: true});
+
+      props.history.push("/profile");
     } catch (err) {
       console.log("Signup failed", err);
     }
   };
 
   return (
-    <div className="App">
+    <div>
       <Switch>
-        <Route
-          path="/signin"
-          render={(routerProps) => {
-            return (
-              <SignIn
-                error={myError}
-                onSignIn={handleSignIn}
-                {...routerProps}
-              />
-            );
-          }}
-        />
         <Route exact path="/" component={HomePage} />
-        <Route
-          path="/signup"
-          render={(routeProps) => {
+        <Route path="/signin" render={(routerProps) => {
+            return <SignIn error={myError} onSignIn={handleSignIn} {...routerProps}/>;
+        }}/>
+        <Route path="/signup" render={(routeProps) => {
             return <SignUp onSignUp={handleSignUp} {...routeProps} />;
-          }}
-        />
+        }}/>
       </Switch>
     </div>
   );
