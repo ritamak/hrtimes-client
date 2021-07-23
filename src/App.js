92fa2e8c552cc83347953d8bd69a2ba0966f21ef
ritamak <ritapmak@gmail.com>
2021-07-23 10:59:34 +0100

    create article working

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App(props) {
   const [myError, updateError] = useState(null);
   const [data, updateData] = useState([]);
   const [interests, updateInterests] = useState([]);
+  const [article, updateArticle] = useState([]);
 
   const handleTopicChange = (newInterests) => {
     updateInterests(newInterests);
@@ -164,6 +165,30 @@ function App(props) {
     }
   };
 
+  const handleCreateArticle = async (event) => {
+    event.preventDefault();
+    const { section, subsection, title, body, created_date, author } =
+      event.target;
+
+    let newArticle = {
+      section: section.value,
+      subsection: subsection.value,
+      title: title.value,
+      body: body.value,
+      created_date: created_date.value,
+      author: author.value,
+    };
+
+    try {
+      let response = await axios.post(`${API_URL}/api/create`, newArticle);
+      updateArticle(response.data);
+
+      props.history.push("/profile");
+    } catch (err) {
+      console.log("Creating Article failed", err);
+    }
+  };
+
   if (fetchingUser) {
     return <p>Loading...</p>;
   }
@@ -234,7 +259,13 @@ function App(props) {
           exact
           path="/create"
           render={(routeProps) => {
-            return <CreateArticle {...routeProps} />;
+            return (
+              <CreateArticle
+                {...routeProps}
+                article={article}
+                onCreateArticle={handleCreateArticle}
+              />
+            );
           }}
         />
       </Switch>
--- src/components/CreateArticle/CreateArticle.jsx
+++ src/components/CreateArticle/CreateArticle.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import "./CreateArticle.css";
 
-export default function CreateArticle() {
-  return <div>Create your article</div>;
+export default function CreateArticle(props) {
+  const { onCreateArticle } = props;
+  return (
+    <div className="createArticle">
+      <form onSubmit={onCreateArticle}>
+        <label htmlFor="Section">Section</label>
+        <input
+          type="text"
+          className="form-control"
+          id="sectionCreate"
+          name="section"
+        />
+        <label htmlFor="SubSection">Sub-Section</label>
+        <input
+          type="text"
+          className="form-control"
+          id="subSectionCreate"
+          name="subsection"
+        />
+        <label htmlFor="Title">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          id="titleCreate"
+          name="title"
+        />
+        <label>
+          Your article:
+          <textarea name="body" />
+        </label>
+        <label htmlFor="Created_Date">Created Date</label>
+        <input
+          type="text"
+          className="form-control"
+          id="createdDateCreate"
+          name="created_date"
+        />
+        <label htmlFor="Author">Author</label>
+        <input
+          type="text"
+          className="form-control"
+          id="authorCreate"
+          name="author"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
