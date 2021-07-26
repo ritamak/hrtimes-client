8f3de9b3da31f203744666637bed4aa9088c1b3a
ritamak <ritapmak@gmail.com>
2021-07-26 11:37:50 +0100

    style for create article

--- src/components/CreateArticle/CreateArticle.jsx
+++ src/components/CreateArticle/CreateArticle.jsx
@@ -1,45 +1,115 @@
 import React from "react";
 import "./CreateArticle.css";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function CreateArticle(props) {
   const { onCreateArticle } = props;
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  const classes = useStyles();
   return (
-    <div className="createArticle">
-      <form onSubmit={onCreateArticle}>
-        <label htmlFor="Section">Section</label>
-        <input
-          type="text"
-          className="form-control"
-          id="sectionCreate"
-          name="section"
-        />
-        <label htmlFor="SubSection">Sub-Section</label>
-        <input
-          type="text"
-          className="form-control"
-          id="subSectionCreate"
-          name="subsection"
-        />
-        <label htmlFor="Title">Title</label>
-        <input
-          type="text"
-          className="form-control"
-          id="titleCreate"
-          name="title"
-        />
-        <label>
-          Your article:
-          <textarea name="body" />
-        </label>
-        <label htmlFor="Created_Date">Created Date</label>
-        <input
-          type="text"
-          className="form-control"
-          id="createdDateCreate"
-          name="created_date"
-        />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <>
+      <Container component="main" maxWidth="s">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <form className={classes.form} noValidate onSubmit={onCreateArticle}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="section"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="section"
+                  label="section"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="subsection"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="subsection"
+                  label="Sub Section"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="title"
+                  label="title"
+                  name="title"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="body"
+                  label="body"
+                  name="body"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="created_date"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="created_date"
+                  label="created date"
+                  autoFocus
+                />
+              </Grid>
+            </Grid>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              style={{ color: "white", background: "black" }}
+            >
+              Create
+            </Button>
+
+            {props.error && <p className="error">{props.error}</p>}
+          </form>
+        </div>
+        <Box mt={5}></Box>
+      </Container>
+    </>
   );
 }
